aa33b9c5408a1bc1340aeb33171051410b821e80
Ragi Yaser Burhum <ragi@burhum.com>
2012-07-21 15:54:54 -0700

    Added map

--- oakland/mainsite/static/js/mainsite.js
+++ oakland/mainsite/static/js/mainsite.js
@@ -4,14 +4,35 @@ function init() {
 
     map = new OpenLayers.Map({
         div: "map",
-        allOverlays: true
+        //allOverlays: true
     });
 
+
     var osm = new OpenLayers.Layer.OSM();
-    var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: false});
+    osm.setIsBaseLayer(true);
+    var gmap = new OpenLayers.Layer.Google("Google Streets");
+    gmap.setIsBaseLayer(true);
+
+    var local_wms = new OpenLayers.Layer.WMS(
+        "ca:alameda - Tiled", "http://localhost:8080/geoserver/ca/wms",
+        {
+           "transparent":"true",
+           "format":"image/png",
+           LAYERS: 'ca:alameda',
+           STYLES: '',
+           tiled: true,
+           tilesOrigin : map.maxExtent.left + ',' + map.maxExtent.bottom
+        },
+        {
+            buffer: 0,
+            displayOutsideMaxExtent: true,
+            isBaseLayer: false,
+            yx : {'EPSG:4326' : true}
+        }
+    );
 
     // note that first layer must be visible
-    map.addLayers([osm, gmap]);
+    map.addLayers([osm, gmap, local_wms]);
 
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomToMaxExtent();
